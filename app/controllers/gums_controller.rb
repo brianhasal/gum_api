94f30c00e8818183cc1463baf3435e1672081bce
Brian Hasal <brian.hasal@gmail.com>
2022-05-07 06:52:40 -0700

    Fin

--- app/controllers/gums_controller.rb
+++ app/controllers/gums_controller.rb
@@ -1,2 +1,40 @@
 class GumsController < ApplicationController
+
+  def index
+    gum = Gum.all
+    render json: gum.as_json
+  end
+
+  def create
+    gum = Gum.new(
+      "name": params["name"],
+      "flavor": params["flavor"],
+      "cost": params["cost"]
+    )
+    gum.save
+    render json: gum.as_json
+  end
+
+  def show
+    gum = Gum.find_by(id: params["id"])
+    render json: gum.as_json
+  end
+
+  def update
+    gum = Gum.find_by(id: params["id"])
+    gum.name = params["name"] || gum.name
+    gum.flavor = params["flavor"] || gum.flavor
+    gum.cost = params["cost"] || gum.cost
+    gum.save
+    render json: gum.as_json
+  end
+
+  def destroy
+    gum = Gum.find_by(id: params["id"])
+    gum.delete
+    render json: {message: "Gum Deleted"}
+  end
+
+
+
 end
--- config/routes.rb
+++ config/routes.rb
@@ -3,4 +3,11 @@ Rails.application.routes.draw do
 
   # Defines the root path route ("/")
   # root "articles#index"
+
+  get "/gums" => "gums#index"
+  post "/gums" => "gums#create"
+  get "/gums/:id" => "gums#show"
+  patch "/gums/:id" => "gums#update"
+  delete "/gums/:id" => "gums#destroy"
+
 end
